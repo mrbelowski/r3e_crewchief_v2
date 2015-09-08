2294afa587f7aa5f1595309bc3604732218c99ec
mrbelowski <jim.britton@yahoo.co.uk>
2015-09-08 14:04:40 +0100

    start adding decoupled state stuff

---
+++ CrewChiefV2/GameStateData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/**
+ * Holds all the data collected from the memory mapped file for the current tick
+ */
+namespace CrewChiefV2
+{
+    class GameStateData
+    {
+    }
+}
